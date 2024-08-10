2dc208c6681f089213f57d27fe8f5410eac4c471
pagiron <pagiron@scl.edu.gt>
2024-08-09 19:48:33 -0600

    Login terminado

---
+++ main.js
@@ -0,0 +1,26 @@
+//Llamado al nodo de DOM
+let root = document.querySelector("#root");
+
+let div_formulario = document.createElement("div");
+div_formulario.className = "div_formulario";
+
+//Lista de datos (String)
+let formulario = [
+    {nombre: "Login", link: "login/login.html"},
+    {nombre: "Formulario De Pedido", link: "Componentes/FormularioDePedido/DePedido.html"},
+    {nombre: "Formulario De Busqueda", link: "Componentes/FormularioDeBusqueda/DeBusqueda.html"},
+    {nombre: "Formulario De Registro", link: "Componentes/FormularioDeRegistro/DeRegistro.html"},
+    {nombre: "Formulario De Contacto", link: "Componentes/FormularioDeContacto/DeContacto.html"},
+    {nombre: "Formulario de comentarios", link:"Componentes/FormularioDecomentarios/Decomentarios"}
+];
+
+formulario.forEach(itemLista => {
+    let div_item = document.createElement("a");
+    div_item.className = "item";
+    div_item.href = itemLista.link;
+    div_item.innerText = itemLista.nombre;
+
+    div_formulario.appendChild(div_item);
+});
+
+root.appendChild(div_formulario);
